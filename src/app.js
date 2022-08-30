9508f34dfde096e67a4ed36a08ccdd7db656969d
calv <kelvinwong0519@gmail.com>
2022-08-29 22:08:10 -0700

    AnimatedPage working

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import React, { Component } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { AnimatePresence } from 'framer-motion';
 
 import Header from './js/header';
 import Home from './js/home';
-import About from './js/about';
 import Projects from './js/projects';
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <div className="main-body">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            {/* <Route path='/home' element={<Home />} /> */}
-            <Route path='/about' element={<About />} />
-            <Route path='/projects' element={<Projects />} />
-          </Routes>
-        </div>
-      </>
-    )
-  }
+function App() {
+  const location = useLocation();
+  return (
+    <>
+      <Header />
+      <AnimatePresence exitBeforeEnter>
+        <Routes key={location.pathname} location={location}>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </AnimatePresence>
+    </>
+  )
 }
+
+export default App;
--- src/js/home.js
+++ src/js/home.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import AnimatedPage from './AnimatedPage'
 
 export default class Home extends Component {
   render() {
     return (
-      <section className="big-text main-body">
+      <AnimatedPage>
+      <section className="text-body main-body">
         <h2>
           Hi, I am
-          <span className="name-text">
+          <span className="bold name-color">
             &nbsp;Calv.
           </span>
         </h2>
-        <div className="bio-text">
-          <p>
-            big fan.
-          </p>
+        <div className="small-text">
+          <div className="text-box">
+            <p>
+              big fan. <br />
+              here's some bio text if you wanted :D
+            </p>
+          </div>
         </div>
       </section>
+      </AnimatedPage>
     )
   }
 }
