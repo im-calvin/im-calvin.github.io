e74379af24e08dc1e7bf5f6bfe49a7722b3d5a51
im-calvin <kelvinwong0519@gmail.com>
2022-10-11 16:25:02 -0700

    added fonts and reupdated app/home in prep for animatedbg

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
-
 import { AnimatePresence } from 'framer-motion';
 
-import React, { useRef, useState, useMemo, useEffect } from 'react';
-import * as THREE from 'three';
-import three from './js/three';
-import { Canvas, useFrame } from 'react-three-fiber';
+import React, { useRef } from 'react';
 
 import Header from './js/header';
 import Home from './js/home';
@@ -15,40 +11,17 @@ function App() {
   const location = useLocation();
   const ref = useRef();
 
-  useEffect(() => {
-    // setLoading(true);
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild(renderer.domElement);
-
-    ref.current.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-    animate();
-  });
   return (
     <>
       <Header />
       <div classNames='main-body'>
-      <AnimatePresence mode='wait'>
-        <div ref={ref} />
-        <Routes key={location.pathname} location={location}>
-          <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Projects />} />
-        </Routes>
+        <AnimatePresence mode='wait'>
+          <Routes key={location.pathname} location={location}>
+            <Route path='/' element={<Home />} />
+            <Route path='/projects' element={<Projects />} />
+          </Routes>
 
-      </AnimatePresence>
+        </AnimatePresence>
       </div>
     </>
   )
--- src/js/header.js
+++ src/js/header.js
@@ -4,20 +4,21 @@ import { Link } from 'react-router-dom';
 export default class Header extends Component {
   render() {
     return (
-      <header className="header flex">
+      <header className="header flex"
+      onclick="document.body.classList.remove('active')">
         <div className="banner-content">
           <button className="banner-button">
-            <Link to="/" className="nav-item button">Home</Link>
+            <Link to="/" className="nav-item button">HOME</Link>
           </button>
           <button className="banner-button">
-            <Link to="/projects" className="nav-item button">Projects</Link>
+            <Link to="/projects" className="nav-item button">PROJECTS</Link>
           </button>
         </div>
         <div className="flex-grow" />
         <div className="banner-content header-bottom">
           <button className="banner-button icon-button git-button">
             <a href="https://github.com/im-calvin" target="_blank" rel="noreferrer">
-              <img className="icon-img" type="image" src="/GitHub-Mark/PNG/Github-Mark-Light-32px.png" alt='github logo' />
+              <img className="icon-img" type="image" src="/GitHub-Mark/PNG/GitHub-Mark-32px.png" alt='github logo' />
             </a>
           </button>
           <span className="banner-button icon-button mail-button">
@@ -27,7 +28,6 @@ export default class Header extends Component {
           </span>
         </div>
       </header>
-
     )
   }
 };
