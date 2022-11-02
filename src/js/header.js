d80b7224925ee8e16f225500b43ecfc15c0974bc
im-calvin <kelvinwong0519@gmail.com>
2022-11-01 18:55:54 -0700

    change pckg.json

--- src/js/header.js
+++ src/js/header.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Header extends Component {
   render() {
     return (
-      <header className="header flex"
-      onclick="document.body.classList.remove('active')">
+      <header className="header flex" onclick="document.body.classList.remove('active')">
         <div className="banner-content">
           <button className="banner-button">
-            <Link to="/" className="nav-item button">HOME</Link>
+            <Link to="/" className="nav-item button">
+              HOME
+            </Link>
           </button>
           <button className="banner-button">
-            <Link to="/projects" className="nav-item button">PROJECTS</Link>
+            <Link to="/projects" className="nav-item button">
+              PROJECTS
+            </Link>
           </button>
         </div>
         <div className="flex-grow" />
         <div className="banner-content header-bottom">
           <button className="banner-button icon-button git-button">
             <a href="https://github.com/im-calvin" target="_blank" rel="noreferrer">
-              <img className="icon-img" type="image" src="/GitHub-Mark/PNG/GitHub-Mark-32px.png" alt='github logo' />
+              <img
+                className="icon-img"
+                type="image"
+                src="/GitHub-Mark/PNG/GitHub-Mark-32px.png"
+                alt="github logo"
+              />
             </a>
           </button>
           <span className="banner-button icon-button mail-button">
@@ -28,6 +36,6 @@ export default class Header extends Component {
           </span>
         </div>
       </header>
-    )
+    );
   }
-};
+}
