3969a70acc727d191a3223a05647a6ab4022c93f
im-calvin <kelvinwong0519@gmail.com>
2023-06-03 13:28:59 -0700

    add gh-pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: "https://im-calvin.github.io",
+  integrations: [react(), tailwind()],
 });
