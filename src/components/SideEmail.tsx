c9be4e5662c90ba821cb7fda051c4dde784c2ab9
im-calvin <kelvinwong0519@gmail.com>
2023-06-06 23:26:48 -0700

    * update email * add firebase hosting

--- src/components/SideEmail.tsx
+++ src/components/SideEmail.tsx
@@ -4,8 +4,8 @@ export default function SideEmail() {
       <a
         style={{ writingMode: "vertical-lr" }}
         className="relative my-10 inline-block pl-1 no-underline transition-transform ease-in-out hover:translate-y-[-3px] focus:translate-y-[-3px]"
-        href="mailto:kelvinwong0519@gmail.com">
-        kelvinwong0519@gmail.com
+        href="mailto:contact@kelvinw.com">
+        contact@kelvinw.com
       </a>
       <div className="m-0 h-20 w-px border border-red-500" />
     </div>
