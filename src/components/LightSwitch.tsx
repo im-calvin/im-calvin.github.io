b34eebc9bf040d4aec9ead03280b4acfef2f4462
im-calvin <kelvinwong0519@gmail.com>
2023-06-08 00:24:30 -0700

    * add prettierrc * clean up icons with iconify for light switch * clean up menu

--- src/components/LightSwitch.tsx
+++ src/components/LightSwitch.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
 
 const ThemeSwitch = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -27,22 +28,42 @@ const ThemeSwitch = () => {
         checked={isDarkMode}
         onChange={toggleTheme}
       />
-      <label htmlFor="theme-switch" className="flex cursor-pointer items-center">
-        {/* <span className="mr-2">{isDarkMode ? null : "☀️"}</span> */}
+      <label
+        htmlFor="theme-switch"
+        className="flex cursor-pointer items-center"
+      >
         <span className="relative inline-block h-6 w-10 rounded-full bg-gray-300 dark:bg-gray-800">
           <span
             className={`absolute inset-1 transition-transform duration-300 ease-in-out ${
               isDarkMode ? "translate-x-5" : "translate-x-1"
-            }`}>
+            }`}
+          >
             <svg
               className="h-4 w-4 -translate-x-1 transform rounded-full bg-indigo-500 text-white shadow"
               fill="currentColor"
-              viewBox="0 0 8 8">
+              viewBox="0 0 8 8"
+            >
               <circle cx="4" cy="4" r="3" />
             </svg>
           </span>
         </span>
-        <span className="mx-2 transition duration-300 ease-in-out">{isDarkMode ? "🌙" : "☀️"}</span>
+        <span
+          className={`mx-2 transition-transform duration-500 ease-in-out ${
+            isDarkMode ? "rotate-[360deg]" : ""
+          }`}
+        >
+          {isDarkMode ? (
+            <Icon
+              icon="material-symbols:dark-mode-outline-rounded"
+              className="hover:animate-spin"
+            />
+          ) : (
+            <Icon
+              icon="material-symbols:light-mode-outline"
+              className="hover:animate-spin"
+            />
+          )}
+        </span>
       </label>
     </div>
   );
--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 import { SwipeableDrawer } from "@mui/material";
 import ResumeButton from "./ResumeButton";
-import pkg from "react-icons/md/index";
-const { MdClose, MdMenu, MdSegment } = pkg;
+import { Icon } from "@iconify/react";
 
 export default function MenuButton() {
   const [state, setState] = useState(false);
 
-  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-    if (
-      event &&
-      event.type === "keydown" &&
-      ((event as React.KeyboardEvent).key === "Tab" ||
-        (event as React.KeyboardEvent).key === "Shift")
-    ) {
-      return;
-    }
-    setState(open);
-  };
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+      setState(open);
+    };
 
   const menuItem = (text: string, href: string) => {
     return <a href={href}>{text}</a>;
@@ -26,15 +26,20 @@ export default function MenuButton() {
   return (
     <>
       <button onClick={toggleDrawer(true)}>
-        <MdSegment className="h-8 w-8" />
+        <Icon icon="gg:menu-right" className="h-8 w-8" />
       </button>
       <SwipeableDrawer
         anchor={"right"}
         open={state}
         onClose={toggleDrawer(false)}
-        onOpen={toggleDrawer(true)}>
+        onOpen={toggleDrawer(true)}
+      >
         <div className="flex h-full w-40 flex-col items-center justify-evenly gap-10 p-4">
-          <MdClose onClick={toggleDrawer(false)} className="h-8 w-8 cursor-pointer" />
+          <Icon
+            icon="ic:round-close"
+            onClick={toggleDrawer(false)}
+            className="h-8 w-8 cursor-pointer"
+          />
           {menuItem("About", "/about")}
           {menuItem("Projects", "/projects")}
           {menuItem("Blog", "/blog")}
