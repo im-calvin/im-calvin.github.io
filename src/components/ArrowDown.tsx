cdc5e5fb66edc41e3f897e4107521b0013df0d7a
im-calvin <kelvinwong0519@gmail.com>
2023-06-18 01:37:14 -0700

    change email to kelvin@kelvinw.com add arrowdown move "about" into index

---
+++ src/components/ArrowDown.tsx
@@ -0,0 +1,32 @@
+import { Icon } from "@iconify/react";
+import { useEffect, useState } from "react";
+
+export default function ArrowDown() {
+  const [visible, setVisible] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleInteraction = () => {
+      setVisible(false);
+    };
+
+    // Attach event listeners for various user interactions (e.g., scroll, click, etc.)
+    window.addEventListener("scroll", handleInteraction);
+    // Add more event listeners as needed
+
+    return () => {
+      // Clean up the event listeners when the component is unmounted
+      window.removeEventListener("scroll", handleInteraction);
+      // Remove other event listeners here
+    };
+  }, []);
+
+  return (
+    <>
+      {visible && (
+        <div className="z-2 fixed bottom-0 left-0 mb-4 flex w-full animate-bounce justify-center opacity-60 transition">
+          <Icon icon="ep:arrow-down-bold" fontSize={30} />
+        </div>
+      )}
+    </>
+  );
+}
