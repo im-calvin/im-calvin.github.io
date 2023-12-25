56e4637a8e2bbe3b78ff0c3a213c8d9c07fdb7a8
im-calvin <kelvinwong0519@gmail.com>
2023-12-24 16:20:10 -0800

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://im-calvin.github.io",
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), tailwind(), mdx(), partytown()]
 });
