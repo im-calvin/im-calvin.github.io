5af3dbd85610f905abb7c2ce79dc742187cc596b
im-calvin <kelvinwong0519@gmail.com>
2024-01-10 23:03:28 -0800

    add framer motion to mobilemenu

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState, useRef } from "react";
 import Hamburger from "hamburger-react";
 import { createPortal } from "react-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function MenuButton() {
-  const [open, isOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
   const mobileMenuRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
@@ -24,22 +25,54 @@ export default function MenuButton() {
     );
   };
 
+  const routes = [
+    { title: "Projects", href: "/projects" },
+    { title: "Experience", href: "/experience" },
+    { title: "Resume", href: "/resume.pdf", target: "_blank" },
+  ];
+
   return (
     <>
       <button className="z-30 hover:text-moona-purple">
-        <Hamburger toggled={open} toggle={isOpen} size={24} />
+        <Hamburger toggled={isOpen} toggle={setOpen} size={24} />
       </button>
-      {open &&
+      {isOpen &&
         createPortal(
-          <div
-            className={`transform transition-all duration-300 ${
-              open ? "scale-100 opacity-100" : "scale-0 opacity-0"
-            } flex flex-col items-center justify-center gap-4 bg-moona-white  dark:bg-anya-darkPurple dark:text-moona-white`}
-          >
-            {menuItem("Projects", "/projects")}
-            {menuItem("Experience", "/experience")}
-            {menuItem("Resume", "/resume.pdf", "_blank")}
-          </div>,
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className="flex flex-col items-center justify-center gap-4 bg-moona-white  dark:bg-anya-darkPurple dark:text-moona-white"
+            >
+              <ul className="grid gap-2">
+                {routes.map((item, idx) => (
+                  <motion.li
+                    initial={{ scale: 0, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 20,
+                      delay: 0.1 + idx / 10,
+                    }}
+                    key={item.title}
+                    className="w-full rounded-xl p-[0.08rem]"
+                  >
+                    <a
+                      onClick={() => setOpen((prev) => !prev)}
+                      className={"flex w-full items-center justify-between p-5"}
+                      href={item.href}
+                      target={item.target}
+                    >
+                      <span className="flex gap-1 text-lg">{item.title}</span>
+                    </a>
+                  </motion.li>
+                ))}
+              </ul>
+            </motion.div>
+          </AnimatePresence>,
           mobileMenuRef.current!
         )}
     </>
