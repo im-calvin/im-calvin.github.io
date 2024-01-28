1436ae4b38958a5dcd564061c3616e365fd780c8
im-calvin <kelvinwong0519@gmail.com>
2024-01-28 00:55:04 -0800

    add cosona text

--- src/components/hackathons/Cosona.tsx
+++ src/components/hackathons/Cosona.tsx
@@ -11,16 +11,25 @@ export default function Cosona() {
     <ProjectExpanded
       title="Cosona"
       titleLink="https://lablab.ai/event/cohere-coral-hackathon/gforce/cosona"
-      description="Developed SaaS platform for a Canadian environmental consulting firm to help improve customers' efficiency in calculating specific scenarios for environmental compliance."
+      description="Created a platform where users upload content to construct a unique persona that they can engage in conversation with. Potential applications could be teaching languages in an engaging way, combatting depression, or making customer service feel more personal."
       implementation={[
-        "Advocated for Docker image deployment to deliver cloud service on Azure Serverless functions within a deadline of 2 weeks",
-        "Spearheaded automated testing with Pytest and Bitbucket Pipelines with 80% code coverage",
-        "Overhauled build and deployment process to Microsoft Azure",
-        "Updated legacy code written in Fortran to Python",
-        "Implemented several scientific algorithms to complete e-tools",
+        "Converted user's documents into a persona using Cohere's Chat API",
+        "Used Langchain to create context-aware responses to user input",
+        "Grounded the persona with the uploaded content by using RAG",
+        "Automated deployment using Docker and GitHub Actions to home server",
       ]}
       key={"cosona"}
-      technologies={["Python", "Docker", "Azure", "Bitbucket"]}
+      technologies={[
+        "Cohere API",
+        "Python",
+        "Langchain",
+        "RAG",
+        "Flask",
+        "Typescript",
+        "Vercel",
+        "GitHub Actions",
+        "Docker",
+      ]}
       imgs={CosonaGallery}
     />
   );
